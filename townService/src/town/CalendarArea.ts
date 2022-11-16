c28111fc49614c81ac6db533a117aac12d125ec9
Manav Sharma <sharma.manav@northeastern.edu>
2022-11-16 16:09:19 -0500

    i switched our type of events to be not be a whole different class (i think)

--- townService/src/town/CalendarArea.ts
+++ townService/src/town/CalendarArea.ts
@@ -3,14 +3,14 @@ import InteractableArea from './InteractableArea';
 import {
   BoundingBox,
   TownEmitter,
+  Event,
   CalendarArea as CalendarAreaModel,
 } from '../types/CoveyTownSocket';
-import CalendarEvent from '../lib/CalendarEvent';
 import Player from '../lib/Player';
 
 export default class CalendarArea extends InteractableArea {
   /* The events in this CalendarArea */
-  private _events: CalendarEvent[];
+  private _events: Event[];
 
   public get events() {
     return this._events;
@@ -19,7 +19,7 @@ export default class CalendarArea extends InteractableArea {
   /**
    * Creates a new ConversationArea
    *
-   * @param event model containing this area's current topic and its ID
+   * @param events model containing this area's current topic and its ID
    * @param coordinates  the bounding box that defines this conversation area
    * @param townEmitter a broadcast emitter that can be used to emit updates to players
    */
@@ -29,7 +29,7 @@ export default class CalendarArea extends InteractableArea {
     townEmitter: TownEmitter,
   ) {
     super(calendarAreaModel.id, coordinates, townEmitter);
-    this._events = calendarAreaModel.events.map(model => CalendarEvent.fromModel(model));
+    this._events = calendarAreaModel.events;
   }
 
   /**
@@ -74,6 +74,6 @@ export default class CalendarArea extends InteractableArea {
   }
 
   public updateModel({ events }: CalendarAreaModel) {
-    this._events = events.map(event => CalendarEvent.fromModel(event));
+    this._events = events;
   }
 }
