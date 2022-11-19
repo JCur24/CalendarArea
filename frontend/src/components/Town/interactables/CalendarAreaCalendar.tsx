0072515db18a872341bfa6e4abc2d774ea2d27ae
John Curanaj <Jcuranaj24@gmail.com>
2022-11-18 19:10:52 -0500

    Added Full Calendar with everything except listeners and playback changes

--- frontend/src/components/Town/interactables/CalendarAreaCalendar.tsx
+++ frontend/src/components/Town/interactables/CalendarAreaCalendar.tsx
@@ -3,11 +3,25 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import { nanoid } from 'nanoid';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons';
+import { CalendarEvent } from '../../../../../shared/types/CoveyTownSocket';
 import {
+  Box,
   Button,
   ButtonGroup,
   Center,
   Container,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
   Popover,
   PopoverCloseButton,
   PopoverContent,
@@ -15,79 +29,109 @@ import {
   PopoverHeader,
   PopoverTrigger,
   Portal,
+  useDisclosure,
 } from '@chakra-ui/react';
-import React from 'react';
 
 export default function CalendarAreaCalendar(): JSX.Element {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [events, setEvents] = useState<CalendarEvent[]>([]);
+  const [newEvent, setNewEvent] = useState<CalendarEvent>();
+
+  function EventModal(): JSX.Element {
+    const [title, setTitle] = useState('');
+
+    return (
+      <Modal isCentered isOpen={isOpen} onClose={onClose}>
+        <ModalContent>
+          <ModalHeader>Create Event</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl>
+              <FormLabel>Title</FormLabel>
+              <Input value={title} onChange={e => setTitle(e.target.value)} />
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              onClick={() => {
+                if (newEvent) {
+                  setEvents([...events, { ...newEvent, title }]);
+                }
+                onClose();
+              }}>
+              Create Event
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    );
+  }
+
   return (
-    <Container className='Full-Calendar'>
-      <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-        headerToolbar={{
-          left: 'prev,next',
-          center: 'title',
-          right: 'dayGridMonth,timeGridWeek,timeGridDay',
-        }}
-        initialView='dayGridMonth'
-        editable={false}
-        selectable
-        selectMirror
-        dayMaxEvents
-        initialEvents={[]}
-        select={event => {
-          const title = prompt('Please enter a new title for your event');
-          const calendarApi = event.view.calendar;
-
-          calendarApi.unselect();
-
-          if (title) {
-            calendarApi.addEvent({
-              id: nanoid(),
-              title,
-              start: event.startStr,
-              end: event.endStr,
-              allDay: event.allDay,
-            });
-          }
-        }}
-        eventContent={event => {
-          return (
-            <div>
-              <b>{event.timeText}</b>
-              <>
-                {' '}
-                Event: <i> {event.event.title}</i>
-              </>
-              <Popover closeOnBlur={false} placement='left'>
-                <>
-                  <PopoverTrigger>
-                    <Button></Button>
-                  </PopoverTrigger>
-                  <Portal>
-                    <PopoverContent>
-                      <PopoverHeader>
-                        <Center>Area you sure you want to delete this event?</Center>
-                      </PopoverHeader>
-
-                      <PopoverFooter alignItems='right' justifyContent='space-between' pb={4}>
-                        <ButtonGroup size='sm'>
-                          <Button
-                            onClick={() => {
-                              event.event.remove();
-                            }}>
-                            Delete
-                          </Button>
-                          <PopoverCloseButton />
-                        </ButtonGroup>
-                      </PopoverFooter>
-                    </PopoverContent>
-                  </Portal>
-                </>
-              </Popover>
-            </div>
-          );
-        }}
-      />
-    </Container>
+    <>
+      <Container className='Full-Calendar'>
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          headerToolbar={{
+            left: 'prev,next',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay',
+          }}
+          initialView='dayGridMonth'
+          selectable
+          selectMirror
+          dayMaxEvents
+          events={events}
+          select={event => {
+            const parsedEvent = JSON.parse(JSON.stringify(event));
+            setNewEvent({ ...parsedEvent, id: nanoid() });
+            onOpen();
+          }}
+          eventContent={currentEvent => {
+            if (currentEvent.view.type !== 'dayGridMonth') {
+              return (
+                <Box>
+                  <Box>
+                    <Popover closeOnBlur={false} placement='right'>
+                      <PopoverTrigger>
+                        <FontAwesomeIcon icon={faX} />
+                      </PopoverTrigger>
+                      <Portal>
+                        <PopoverContent>
+                          <PopoverHeader>
+                            <Center>Area you sure you want to delete this event?</Center>
+                          </PopoverHeader>
+                          <PopoverFooter alignItems='right' justifyContent='space-between' pb={4}>
+                            <ButtonGroup size='sm'>
+                              <Button
+                                onClick={() => {
+                                  setEvents(
+                                    events.filter(event => event.id !== currentEvent.event.id),
+                                  );
+                                }}>
+                                Delete
+                              </Button>
+                              <PopoverCloseButton />
+                            </ButtonGroup>
+                          </PopoverFooter>
+                        </PopoverContent>
+                      </Portal>
+                    </Popover>{' '}
+                    <b> {currentEvent.timeText}</b>
+                  </Box>
+                  {currentEvent.view.type === 'timeGridDay' && (
+                    <Center>
+                      <b>Event: </b>
+                      <i> {currentEvent.event.title}</i>
+                    </Center>
+                  )}
+                </Box>
+              );
+            }
+          }}
+        />
+      </Container>
+      <EventModal />
+    </>
   );
 }
