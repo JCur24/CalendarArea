10a455ee708dcddcdcee6a28b16c57d9f94dfc50
Manav Sharma <sharma.manav@northeastern.edu>
2022-11-21 14:04:48 -0500

    added some testing stuff

--- townService/src/town/CalendarArea.test.ts
+++ townService/src/town/CalendarArea.test.ts
@@ -1,5 +1,6 @@
 import { mock, mockClear } from 'jest-mock-extended';
 import { nanoid } from 'nanoid';
+import { Event } from 'socket.io';
 import CalendarEvent from '../lib/CalendarEvent';
 import Player from '../lib/Player';
 import { getLastEmittedEvent } from '../TestUtils';
@@ -12,11 +13,15 @@ describe('CalendarArea', () => {
   const townEmitter = mock<TownEmitter>();
   const id = nanoid();
   let events: CalendarEvent[];
+  let event1: Event;
+  let event2: Event;
+  let event3: Event;
+  let event4: Event;
   let newPlayer: Player;
 
   beforeEach(() => {
     mockClear(townEmitter);
-    // `events = 
+    event1 = {}
     testArea = new CalendarArea({ id, events }, testAreaBox, townEmitter);
     newPlayer = new Player(nanoid(), mock<TownEmitter>());
     testArea.add(newPlayer);
